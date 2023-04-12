1653b2826259ad0b612a5e751545ac0eb636695d
Rizwan Ahmed <rizwanahmedg2020@gmail.com>
2023-04-12 14:05:31 +0500

    install tailwind framwork

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 
 import './App.css'
+import Hello from './Hello'
 
 function App() {
 
   return (
     <div className="App">
-     
+     <Hello />
     </div>
   )
 }
---
+++ src/Hello.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Hello() {
+  return (
+    <div>
+      <h1 className='text-fuchsia-500 text-9xl'>Hello World</h1>
+    </div>
+  )
+}
